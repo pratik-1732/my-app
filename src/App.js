15f7d937f10811e82be37e068ff2f270876babb8
Pratik <patilpratikp035@gmail.com>
2024-05-21 18:27:35 +0530

    image folder to public file adding shuffle card function and function for counting turns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImg = [
+  { src: "/images/Arya Stark.jpg" },
+  { src: "/images/Daenerys Targaryen.jpg" },
+  { src: "/images/Jaime.jpg" },
+  { src: "/images/Jon Snow.jpg" },
+  { src: "/images/Missandei.jpg" },
+  { src: "/images/Night King.jpg" },
+  { src: "/images/Theon.jpg" },
+  { src: "/images/Tyrion.jpg" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // cards shuffle
+  const cardsShuffle = () => {
+    const shuffleCards = [...cardImg, ...cardImg]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffleCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
   return (
     <div className="App">
       <h1>Mind Game</h1>
-      <button>New Game</button>
+      <button onClick={cardsShuffle}>New Game</button>
     </div>
   );
 }
