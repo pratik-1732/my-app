2d982c7a7e0cf25b3eb30634d72b0050389bdcaa
Pratik <patilpratikp035@gmail.com>
2024-05-26 12:15:43 +0530

    front card height and width change

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -22,7 +22,7 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
           src="/images/cover img.png"
           alt="card back"
           onClick={handleClick}
-          style={{ height: "12rem", width: "12rem" }}
+          style={{ height: "10rem", width: "10rem" }}
         />
       </div>
     </div>
